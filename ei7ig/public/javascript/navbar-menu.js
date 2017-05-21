a92fb17d9888885185550139c1dfef45a7a2c155
John Ronan <jpronans@users.noreply.github.com>
2017-05-21 22:31:07 +0100

    Open AREN site in new page

--- ei7ig/public/javascript/navbar-menu.js
+++ ei7ig/public/javascript/navbar-menu.js
@@ -5,10 +5,11 @@ $(function () {
       "<a class='ui item' href='index.html'>Home</a>" +
       "<a class='ui item' href='shack.html'>Shack</a>" +
       "<a class='ui item' href='aprs.html'>APRS</a>" +
-      "<a class='ui item' href='aren.html'>AREN</a>" +
+      //"<a class='ui item' href='aren.html'>AREN</a>" +
       //"<a class='ui item' href='myaren.html'>My AREN</a>" +
       "<a class='ui item' href='sat.html'>Satellite</a>" +
       "<a class='ui item' href='dmr.html'>DMR</a>" +
+      "<a class='ui item' href='http://aren.ie'>AREN</a>" +
       "<a class='ui item' href='data.html'>WX Data</a>" +
       "<a class='ui item' href='logs.html'>Log</a>" +
       "<a class='ui item' href='links.html'>Links</a>" +
